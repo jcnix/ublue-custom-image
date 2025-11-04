e3f2a6dc9f2344f87789b60c1eccf191657ee3d6
Casey Jones <jonescaseyb@gmail.com>
2025-11-03 20:12:14 -0600

    update build

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,23 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+#replace Fedora kernel with patched kernel
+
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
+
+dnf5 -y copr enable cjones745/kernel-amdgpu-patch
+dnf5 -y install kernel-6.17.6-301.fc43
+dnf5 -y copr disable cjones745/kernel-amdgpu-patch
+
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+export DRACUT_NO_XATTR=1
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
